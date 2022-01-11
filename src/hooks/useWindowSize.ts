442719d7c7c622194f23916277a6bb9de5a4d699
Matt <matt@Matts-MacBook-Pro.local>
2022-01-11 22:22:29 +0100

    useWindowSize hotfix

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useOnResize from "./useOnResize";
 
 type Size = Parameters<Parameters<typeof useOnResize>[0]>[0];
@@ -6,6 +6,10 @@ type Size = Parameters<Parameters<typeof useOnResize>[0]>[0];
 const useWindowSize = () => {
   const [size, setSize] = useState<Size>();
   useOnResize(setSize);
+
+  useEffect(() => {
+    setSize({ width: window.innerWidth, height: window.innerHeight });
+  }, []);
 
   return size;
 };
